09b066a1b2f185b41dfb88f6092a89f220317497
kevlarr <6306496+kevlarr@users.noreply.github.com>
2020-12-16 13:50:46 -0500

    Update 1.2.0 checksum

--- Formula/jrny.rb
+++ Formula/jrny.rb
@@ -5,7 +5,7 @@ class Jrny < Formula
   desc "A lightweight, compiled, zero-dependency PostgreSQL schema revision tool; just add SQL! "
   homepage "https://github.com/kevlarr/jrny"
   url "https://github.com/kevlarr/jrny/releases/download/v1.2.0/jrny-mac.tar.gz"
-  sha256 "37d7b88808796b229c757af952ae46400484f9c6ffb0f2199d7561dd40459c75"
+  sha256 "df6c5a0ebc2a4ec0d0e79d22f5cd214f3cd4683a2f13ab14c203a8c07563ecaa"
   version "1.2.0"
 
   def install
